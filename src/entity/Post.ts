d5577b859520c591d44251f70c0d6dd98a592209
Isabela Rosa <isabelarosa954@gmail.com>
2024-11-18 03:39:26 -0300

    create entity user and post

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,24 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { IsString, IsNotEmpty, MaxLength } from "class-validator";
+import { User } from './User';
 
-//TODO Crie a entidade de Post
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    title?: string;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    description?: string;
+
+    @ManyToOne(() => User, user => user.id)
+    userId?: User;
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { IsString, IsNotEmpty, MaxLength, IsEmail } from "class-validator";
+import { Post } from "./Post";
 
-//TODO Crie a entidade de User
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    firstName?: string;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    lastName?: string;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email?: string;
+
+    @OneToMany(() => Post, post => post.userId)
+    posts?: Post[];
+}
